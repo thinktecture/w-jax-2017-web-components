a7fd8a29b905117cf89bf74b80705db547fdd6c8
Manuel Rauber <manuel.rauber@thinktecture.com>
2017-11-09 11:56:02 +0100

    prepare livecoding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,16 @@
+import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
-
-import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './components/app/app.component';
-import {HomeComponent} from './components/home/home.component';
-import {NumberPadComponent} from './components/numberPad/numberPad.component';
-import {NumberPadContainerComponent} from './components/numberPad/numberPadContainer.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import {NumberPadShadowComponent} from './components/numberPadShadow/numberPadShadow.component';
-import {NumberPadContainerShadowComponent} from './components/numberPadShadow/numberPadContainerShadow.component';
-import {NumberPadContainerPolymerComponent} from './components/numberPadPolymer/numberPadContainerPolymer.component';
-import {SingleSlotComponent} from './components/slots/singleSlot.component';
-import {NumberPadSlotComponent} from './components/numberPad/numberPadSlot.component';
-import {NumberPadShadowSlotComponent} from './components/numberPadShadow/numberPadShadowSlot.component';
-import {MultiSlotComponent} from './components/slots/multiSlot.component';
-import {SlotContainerComponent} from './components/slots/slotContainer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    NumberPadComponent,
-    NumberPadContainerComponent,
-    NumberPadShadowComponent,
-    NumberPadContainerShadowComponent,
-    NumberPadContainerPolymerComponent,
-    SingleSlotComponent,
-    MultiSlotComponent,
-    NumberPadSlotComponent,
-    NumberPadShadowSlotComponent,
-    SlotContainerComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA
-  ]
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
